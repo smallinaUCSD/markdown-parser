cd1c344c1c3d1b225bf1d5814c9abe5b8cd43491
Seshu <80934775+smallinaUCSD@users.noreply.github.com>
2022-04-24 12:09:32 -0700

    fixed bug )[ and ! before image

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,63 +10,69 @@ import java.net.URL;
 
 public class MarkdownParse {
 
-	public static ArrayList<String> getLinks(String markdown) {
-		ArrayList<String> toReturn = new ArrayList<>();
-		// find the next [, then find the ], then find the (, then read link upto next )
-		int currentIndex = 0;
-		while (currentIndex < markdown.length()) {
+    public static ArrayList<String> getLinks(String markdown) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        // find the next [, then find the ], then find the (, then read link upto next )
+        int currentIndex = 0;
+        while (currentIndex < markdown.length()) {
 
-			int openBracket = markdown.indexOf("[", currentIndex);
+            int openBracket = markdown.indexOf("[", currentIndex);
 
-			// quit if no [, found
-			if (openBracket == -1)
-				break;
+            // quit if no [, found
+            if (openBracket == -1)
+                break;
 
-			int closeBracket = markdown.indexOf("]", openBracket);
-			int openParen = markdown.indexOf("(", closeBracket);
-			int closeParen = markdown.indexOf(")", openParen);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            int exclamation = markdown.indexOf("!", currentIndex);
+            
+            // add to list ONLY if ALL 4 [,],(,), are found
+            if (openBracket != -1 && closeBracket != -1 && openParen != -1 && closeParen != -1) {
+                String url = markdown.substring(openParen + 1, closeParen);
+                // validate if URL is NOT null and is either valid image or URL, ![ for an image
+                if (url != null && (((exclamation != -1 && exclamation < openBracket) && isValidImage(url)) 
+                || isValidURL(url)))
+                    toReturn.add(url);
+            }
+            
+            // point currentIndex to next openBracket
+            if ((closeParen != -1) && (closeParen > openBracket)) 
+                currentIndex = closeParen + 1;
+            else if ((closeBracket != -1) && (closeBracket > openBracket))
+                currentIndex = closeBracket + 1;
+            else
+                currentIndex++;
+        }
+        return toReturn;
+    }
 
-			// add to list ONLY if ALL 4 [,],(,), are found
-			if (openBracket != -1 && closeBracket != -1 && openParen != -1 && closeParen != -1) {
-				String url = markdown.substring(openParen + 1, closeParen);
-				// validate if url is not null and valid link or image
-				if (url != null && (isValidLink(url) || isValidImage(url)))
-					toReturn.add(url);
-			}
-			if (closeParen != -1) // if link/image is specified
-				currentIndex = closeParen + 1;
-			else // if no link/image is specified
-				currentIndex = closeBracket + 1;
-		}
-		return toReturn;
-	}
+    static boolean isValidURL(String url) {
+        String validURL = "((http|https)://)(www.)?"
+                + "[a-zA-Z0-9@:%._\\+~#?&//=]"
+                + "{2,256}\\.[a-z]"
+                + "{2,6}\\b([-a-zA-Z0-9@:%"
+                + "._\\+~#?&//=]*)";
+        return url.matches(validURL);
+    }
 
-	static boolean isValidLink(String url) {
-		String validURL = "((http|https)://)(www.)?"
-				+ "[a-zA-Z0-9@:%._\\+~#?&//=]"
-				+ "{2,256}\\.[a-z]"
-				+ "{2,6}\\b([-a-zA-Z0-9@:%"
-				+ "._\\+~#?&//=]*)";
-		return url.matches(validURL);
-	}
+    static boolean isValidImage(String url) {
+        Image image;
+        try {
+            if (isValidURL(url)) {
+                image = ImageIO.read(new URL(url));
+            } else
+                image = ImageIO.read(new File(url));
+        } catch (Exception e) {
+            return false;
+        }
+        return (image != null);
+    }
 
-	static boolean isValidImage(String url) {
-		Image image;
-		try {
-			if (url.contains("http")) {
-				image = ImageIO.read(new URL(url));
-			} else
-				image = ImageIO.read(new File(url));
-		} catch (Exception e) {
-			return false;
-		}
-		return (image != null);
-	}
-
-	public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-		String content = Files.readString(fileName);
-		ArrayList<String> links = getLinks(content);
-		System.out.println(links);
-	}
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+        System.out.println(links);
+    }
 }
