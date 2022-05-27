8a7e9227dda268278735531bcec0aaa35391ace3
Seshu <80934775+smallinaUCSD@users.noreply.github.com>
2022-05-27 14:37:03 -0700

    update

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MarkdownParseTest {
@@ -11,20 +11,20 @@ public class MarkdownParseTest {
     @Test
     public void testFile1() throws IOException {
         String contents= Files.readString(Path.of("Snippet1.md"));
-        List<String> expect = List.of();
+        List<String> expect = List.of("google.com");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
     @Test
     public void testFile2() throws IOException {
         String contents= Files.readString(Path.of("Snippet2.md"));
-        List<String> expect = List.of();
+        List<String> expect = List.of("a.com","a.com","example.com");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
     @Test
     public void testFile3() throws IOException {
         String contents= Files.readString(Path.of("Snippet3.md"));
-        List<String> expect = List.of("https://www.twitter.com","https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule","https://cse.ucsd.edu/");
+        List<String> expect = List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
-    
+
 }
