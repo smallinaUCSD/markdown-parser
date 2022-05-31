8b93e58f762128c99eda93166e3a83fc0c434f56
Seshu <80934775+smallinaUCSD@users.noreply.github.com>
2022-05-30 21:57:25 -0700

    update

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,21 +10,14 @@ public class MarkdownParseTest {
 
     @Test
     public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("Snippet1.md"));
-        List<String> expect = List.of("google.com");
+        String contents= Files.readString(Path.of("Test4.md"));
+        List<String> expect = List.of();
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
     @Test
     public void testFile2() throws IOException {
-        String contents= Files.readString(Path.of("Snippet2.md"));
-        List<String> expect = List.of("a.com","a.com","example.com");
+        String contents= Files.readString(Path.of("Test5.md"));
+        List<String> expect = List.of("https://spec.commonmark.org/url");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
-    @Test
-    public void testFile3() throws IOException {
-        String contents= Files.readString(Path.of("Snippet3.md"));
-        List<String> expect = List.of("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
-    }
-
 }
